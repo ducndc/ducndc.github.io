5eaca6129c2413a5e76c0446a1d60cbdf1b25d60
ducndc <nguyendangchungduc1999@gmail.com>
2026-06-17 22:11:40 +0700

    CCI 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 
-// import mdx from '@astrojs/mdx';
+//import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import remarkMath from "remark-math";
@@ -10,7 +10,7 @@ import rehypeKatex from "rehype-katex";
 export default defineConfig({
 	site: 'https://ducndc.github.io',
 	base: '/',
-	integrations: [mdx(), sitemap()],
+	integrations: [sitemap()],
 	markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
