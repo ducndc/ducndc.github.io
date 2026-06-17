1cb05704aa5bd3d8ff10226928f29cf6c9ba025b
ducndc <nguyendangchungduc1999@gmail.com>
2026-06-17 22:04:50 +0700

    CCI 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
+// import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import remarkMath from "remark-math";
