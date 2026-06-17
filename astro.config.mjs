79ec261c68f0ae02a94d3acc6b361816b8d03075
ducndc <nguyendangchungduc1999@gmail.com>
2026-06-17 22:00:09 +0700

    CCI 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-import { defineConfig } from "astro/config";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
